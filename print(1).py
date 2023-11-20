24708cff6ec92ec66e480c1d9068c7727affdd5c
samcarmonaaa <151482869+samcarmonaaa@users.noreply.github.com>
2023-11-19 20:49:20 -0600

    Update print(1).py

--- print(1).py
+++ print(1).py
@@ -4,31 +4,41 @@
 
 import re
 
+# Clase para la tabla de símbolos
 class TablaDeSimbolos:
     def __init__(self):
+        # Diccionario para almacenar símbolos (nombre -> tipo)
         self.simbolos = {}
 
     def insertar(self, nombre, tipo):
+        # Método para insertar un nuevo símbolo en la tabla
         self.simbolos[nombre] = tipo
 
     def buscar(self, nombre):
+        # Método para buscar un símbolo en la tabla por su nombre
         return self.simbolos.get(nombre, None)
 
     def eliminar(self, nombre):
+        # Método para eliminar un símbolo de la tabla por su nombre
         if nombre in self.simbolos:
             del self.simbolos[nombre]
 
+# Clase para el analizador semántico
 class AnalizadorSemantico:
     def __init__(self, tabla_de_simbolos):
+        # Inicialización del analizador semántico con una tabla de símbolos
         self.tabla_de_simbolos = tabla_de_simbolos
+        # Lista para almacenar mensajes de error
         self.errores = []
 
     def analizar_codigo(self, codigo_fuente):
+        # Método para realizar el análisis semántico del código fuente
         lineas = codigo_fuente.split('\n')
         self.errores = []
         funcion_actual = None
 
         def reportar_error(mensaje, numero_de_linea):
+            # Función interna para agregar un mensaje de error a la lista
             self.errores.append(f"Error - Línea {numero_de_linea}: {mensaje}")
 
         for i, linea in enumerate(lineas, start=1):
@@ -73,6 +83,7 @@ class AnalizadorSemantico:
 
         return self.errores
 
+# Función para leer el código fuente desde un archivo
 def leer_codigo_fuente(ruta_archivo):
     try:
         with open(ruta_archivo, 'r') as archivo:
@@ -82,6 +93,7 @@ def leer_codigo_fuente(ruta_archivo):
         print(f"El archivo '{ruta_archivo}' no se encontró.")
         return None
 
+# Punto de entrada principal
 if __name__ == "__main__":
     ruta_archivo = "codigo_fuente.txt"  # Reemplaza con la ruta de tu archivo
     codigo_fuente = leer_codigo_fuente(ruta_archivo)
@@ -96,5 +108,3 @@ if __name__ == "__main__":
                 print(error)
         else:
             print("El código fuente es correcto.")
-
-            
